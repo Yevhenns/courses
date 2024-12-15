736839a9b483bd98f85b124240ca446bde8e184d
Yevhenns <gifestum@gmail.com>
2024-12-15 11:21:16 +0200

    selector markup

--- components/AppCourseSelector.vue
+++ components/AppCourseSelector.vue
@@ -1,3 +1,39 @@
 <template>
-    <img src="/assets/images/flag.png" width="44" height="44" alt="flag" />
+    <div class="courseSelector">
+        <img src="/assets/images/flag.png" width="44" height="44" alt="flag" />
+        <div>
+            <button class="selectorButton">
+                <span>Польська</span>
+                <Icon
+                    name="mynaui:chevron-down-solid"
+                    style="color: #3a89fd"
+                    size="24"
+                />
+            </button>
+            <p class="selectorInfo">Лікар (В2)</p>
+        </div>
+    </div>
 </template>
+
+<style scoped>
+.courseSelector {
+    display: flex;
+    gap: 10px;
+    align-items: center;
+}
+
+.selectorButton {
+    display: flex;
+    align-items: center;
+    padding: 0;
+    border: none;
+    background-color: transparent;
+    color: #3a89fd;
+    font-size: 22px;
+    font-weight: 600;
+}
+
+.selectorInfo {
+    color: #4e4e4e;
+}
+</style>
