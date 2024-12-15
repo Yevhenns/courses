088569b8a6a68614eb93bcacbcc59f3e66574984
Yevhenns <gifestum@gmail.com>
2024-12-15 12:22:06 +0200

    added text to item

--- components/AppLessonsListItem.vue
+++ components/AppLessonsListItem.vue
@@ -7,7 +7,13 @@ defineProps<{
 </script>
 
 <template>
-    <div class="lessonsListItem"><img :src="img" /></div>
+    <div class="lessonsListItem">
+        <img :src="img" width="80" height="80" alt="lesson image" />
+        <div>
+            <p class="lessonTitle">{{ title }}</p>
+            <p class="lessonDescription">{{ description }}</p>
+        </div>
+    </div>
 </template>
 
 <style scoped>
@@ -16,5 +22,22 @@ defineProps<{
     width: 100%;
     height: auto;
     border-radius: 16px;
+    display: flex;
+    gap: 20px;
+    align-items: center;
+    box-shadow: 0px 2px 7px 0px rgba(216, 107, 31, 0.14);
+}
+
+.lessonTitle {
+    background-color: inherit;
+    color: #a0a0a0;
+    margin-bottom: 4px;
+}
+
+.lessonDescription {
+    background-color: inherit;
+    color: #793708;
+    font-size: 18px;
+    font-weight: 700;
 }
 </style>
