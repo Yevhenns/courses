cdabce0c09be0675eb7a64cb18d5181dcaabe459
yevhenns <gifestum@gmail.com>
2024-12-16 18:38:26 +0200

    popover hot fix

--- components/AppPopover.vue
+++ components/AppPopover.vue
@@ -1,21 +1,21 @@
 <script setup lang="ts"></script>
 
 <template>
-    <div class="popover">
-        <slot></slot>
-    </div>
+  <div class="popover">
+    <slot></slot>
+  </div>
 </template>
 
 <style scoped>
 @import "/assets/css/variables.css";
 
 .popover {
-    border: var(--border);
-    width: calc(100vw - 100px);
-    min-height: calc(100vh - 112px);
-    background-color: var(--white-color);
-    border-radius: 8px;
-    padding: 12px;
-    z-index: 2;
+  border: var(--border);
+  width: 300px;
+  min-height: calc(100vh - 112px);
+  background-color: var(--white-color);
+  border-radius: 8px;
+  padding: 12px;
+  z-index: 2;
 }
 </style>
